63390b8d4b82d5b813a65c98975b9097dd194b3e
ceudenascimento <ceudenascimento@gmail.com>
2024-08-18 20:52:56 -0300

    atividade 02 site estatico react no springboot

---
+++ atividade02spring/src/main/java/com/atvstatic/atividade02spring/Atividade02springApplication.java
@@ -0,0 +1,13 @@
+package com.atvstatic.atividade02spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade02springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade02springApplication.class, args);
+	}
+
+}
---
+++ atividade02spring/src/test/java/com/atvstatic/atividade02spring/Atividade02springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvstatic.atividade02spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade02springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
